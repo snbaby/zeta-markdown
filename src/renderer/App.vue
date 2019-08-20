2a9e3c3329a2ca6218aa35315e73822bb9f2ad74
sn_baby <sn_baby@qq.com>
2019-08-20 21:42:41 +0800

    初始化

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'zeta-markdown'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
